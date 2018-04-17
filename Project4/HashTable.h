a017bb475316d0091139ff3b2ae48615f88a08ec
Aashish Bharadwaj <aashish.bharadwaj01@gmail.com>
2018-04-17 16:35:23 -0400

    Implement inline HashTable class

---
+++ Project4/HashTable.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "Slot.h"	//No cpp file because the requirements/instructions were contradictory
+
+template <class T> class HashTable{
+public:
+	HashTable(){
+
+	}
+	~HashTable(){
+
+	}
+
+	bool HashTable<T>::insert(int key, T value, int& collisions){
+
+	}
+
+	bool HashTable<T>::remove(int key){
+
+	}
+
+	bool HashTable<T>::find(int key, T& value){
+
+	}
+
+	float HashTable<T>::alpha(){
+
+	}
+};
