3c7102a1b4285dd0432b00b1074b5f79bb15b056
Aashish Bharadwaj <aashish.bharadwaj01@gmail.com>
2018-04-17 16:01:06 -0400

    Reformat Slot.h

--- Project4/Slot.h
+++ Project4/Slot.h
@@ -7,7 +7,9 @@ using namespace std;
 
 // There are three types of slots in a closed hash:
 // Normal (full) slots, empty slots, and tombstones
-enum SlotType {normalSlot, emptySlot, tombstone};
+enum SlotType{
+	normalSlot, emptySlot, tombstone
+};
 
 
 // Each record holds an integer key and a value of any type
@@ -21,8 +23,7 @@ enum SlotType {normalSlot, emptySlot, tombstone};
 // that deep copies are made upon assignment, and that
 // any values allocated are properly deleted in the Record
 // destructor.
-template <class T> class Slot
-{
+template <class T> class Slot{
 private:
 	int key;
 	T value;
@@ -34,8 +35,7 @@ public:
 	// A possibly better way to keep track of tombstones and emtpy
 	// records would be to use inheritance, similar to the way we
 	// avoided having null pointers in leaf nodes for binary trees.
-	Slot()
-	{
+	Slot(){
 		key = 0;
 		type = emptySlot;
 	}
@@ -43,54 +43,51 @@ public:
 	// This constructor uses an initialization list
 	// See "member initialization" at: http://www.cplusplus.com/doc/tutorial/classes/
 	Slot(int newkey, T newvalue)
-		: key(newkey), value(newvalue)
-	{
+		: key(newkey), value(newvalue){
 		type = normalSlot;
 	}
 
 	// Convert a record to a tombstone
-	void kill() {
+	void kill(){
 		type = tombstone;
 	}
 
 	// Get the integer key of a record
-	int getKey() const {
+	int getKey() const{
 		return key;
 	}
 
 	// Get the value of a record
-	T getValue() const {
+	T getValue() const{
 		return value;
 	}
 
 	// Check if a record is empty
-	bool isEmpty() const {
+	bool isEmpty() const{
 		return(type == emptySlot);
 	}
 
 	// Check if a record is a normal record
-	bool isNormal() const {
+	bool isNormal() const{
 		return(type == normalSlot);
 	}
 
 	// Check if a record is a tombstone
-	bool isTombstone() const {
+	bool isTombstone() const{
 		return (type == tombstone);
 	}
 
 	// Overload the << operator for printing records
-	friend ostream& operator<<(ostream& os, const Slot& me) {
-		if (me.isTombstone())
+	friend ostream& operator<<(ostream& os, const Slot& me){
+		if(me.isTombstone())
 			os << "<<Tombstone>>";
-		else if (me.isEmpty())
+		else if(me.isEmpty())
 			os << "<<Empty>>";
 		else
 			os << "Key: " << me.key << ", Value: " << me.value;
 		return os;
 	}
 
-	~Slot()
-	{
-	}
+	~Slot(){}
 };
 
