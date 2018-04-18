0ba61dfa696ea0dec22db8d456f0dc7742d84128
Aashish Bharadwaj <aashish.bharadwaj01@gmail.com>
2018-04-18 17:39:38 -0400

    Add some stuff

--- Project4/HashTable.h
+++ Project4/HashTable.h
@@ -1,36 +1,69 @@
 #pragma once
 #include "Slot.h"	//No cpp file because the requirements/instructions were contradictory
-#define MAXHASH 10;	//TODO - change to 1000
+#define MAXHASH 10	//TODO - change to 1000
 
 template <class T> class HashTable{
+private:
+	unsigned int pseudoRandomTable[MAXHASH];
+	Slot<T> theTable[MAXHASH];
+
 public:
 	HashTable(){
-
+		GenerateRandomTable();
+		GenerateIteratedTable();
 	}
 	~HashTable(){
 
+	}
+
+	void GenerateRandomTable(){
+		for(unsigned int i = 0; i < MAXHASH; i++){
+			pseudoRandomTable[i] = i;
+		}
+		for(unsigned int i = 0; i < MAXHASH; i++){
+			swapTwoValues(pseudoRandomTable[i], pseudoRandomTable[rand() % MAXHASH]);
+		}
+	}
+	void GenerateIteratedTable(){
+		for(unsigned int i = 0; i < MAXHASH; i++){
+			theTable[i] = Slot<T>();
+		}
 	}
 
 	bool keyExists(int key){
 		return false;
 	}
 
-	bool HashTable<T>::insert(int key, T value, int& collisions){
+	bool insert(int key, T value, int& collisions){
+		unsigned int spot;
+		do{
+			spot = dumbHash(key + spot);
+		}
+		while((theTable[spot]).isEmpty());
+	}
+
+	bool remove(int key){
 
 	}
 
-	bool HashTable<T>::remove(int key){
+	bool find(int key, T& value){
 
 	}
 
-	bool HashTable<T>::find(int key, T& value){
-
-	}
-
-	float HashTable<T>::alpha(){
-
+	float alpha(){
+		return 0;
 	}
 };
+
+void swapTwoValues(unsigned int& value1, unsigned int& value2){
+	unsigned int temp = value1;
+	value1 = value2;
+	value2 = temp;
+}
+
+unsigned int dumbHash(int key){
+	return key % MAXHASH;
+}
 
 unsigned int ELFHash(const char* str, unsigned int length){	//http://www.partow.net/programming/hashfunctions/#ELFHashFunction
 	unsigned int hash = 0;
