de88818a0160c093d3d786c2e7d74b7d8bba2bf8
Aashish Bharadwaj <aashish.bharadwaj01@gmail.com>
2018-04-18 15:47:47 -0400

    Import elfhash

--- Project4/HashTable.h
+++ Project4/HashTable.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Slot.h"	//No cpp file because the requirements/instructions were contradictory
+#define MAXHASH 10;	//TODO - change to 1000
 
 template <class T> class HashTable{
 public:
@@ -8,6 +9,10 @@ public:
 	}
 	~HashTable(){
 
+	}
+
+	bool keyExists(int key){
+		return false;
 	}
 
 	bool HashTable<T>::insert(int key, T value, int& collisions){
@@ -26,3 +31,21 @@ public:
 
 	}
 };
+
+unsigned int ELFHash(const char* str, unsigned int length){	//http://www.partow.net/programming/hashfunctions/#ELFHashFunction
+	unsigned int hash = 0;
+	unsigned int x = 0;
+	unsigned int i = 0;
+
+	for(i = 0; i < length; ++str, ++i){
+		hash = (hash << 4) + (*str);
+
+		if((x = hash & 0xF0000000L) != 0){
+			hash ^= (x >> 24);
+		}
+
+		hash &= ~x;
+	}
+
+	return hash;
+}
